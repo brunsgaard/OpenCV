28a58f83d2842648cae6ee00c5a3b79538c97d8a
Bo Andersen <boa@e-conomic.com>
2018-01-08 10:35:55 +0100

    Minimum program

--- Program.cs
+++ Program.cs
@@ -9,8 +9,7 @@ namespace foo
         {
             Console.WriteLine("Hello World!");
             var mat = new Mat(100, 100, MatType.CV_8UC1, new Scalar(0));
-            Cv2.ImShow("test", mat);
-            Cv2.WaitKey();
+	    Console.ReadLine();
         }
     }
 }
